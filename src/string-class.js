64fe8973672b24c2fba74b1990a045d4fb2d6180
andela-tisrael <israel.tomilayo@andela.com>
2016-12-19 00:12:02 +0100

    write code to pass test for the string manipulatio

--- src/string-class.js
+++ src/string-class.js
@@ -1,19 +1,23 @@
 'use strict';
 const StringClass = {
   hasVowels() {
-
+    return /[aeiouAEIOU]/g.test( this );
   },
 
   toUpper() {
-
+    return this.replace( /[a-z]/g, ( character ) => {
+      return String.fromCharCode( character.charCodeAt() - 32 );
+    } );
   },
 
   toLower() {
-
+    return this.replace( /[A-Z]/g, ( character ) => {
+      return String.fromCharCode( character.charCodeAt() + 32 );
+    } );
   },
 
   ucFirst() {
-
+    return `${this.slice(0,1).toUpper()}${this.slice(1).toLower()}`;
   },
 
   isQuestion() {
