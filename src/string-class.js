4947833bfc1a51ee993af857202adc4945fcaa82
andela-tisrael <israel.tomilayo@andela.com>
2016-12-18 23:44:37 +0100

    write test for the string manipulation all tests are failing

---
+++ src/string-class.js
@@ -0,0 +1,66 @@
+'use strict';
+const StringClass = {
+  hasVowels() {
+
+  },
+
+  toUpper() {
+
+  },
+
+  toLower() {
+
+  },
+
+  ucFirst() {
+
+  },
+
+  isQuestion() {
+
+  },
+
+  word() {
+
+  },
+
+  wordCount() {
+
+  },
+
+  reverseWord() {
+
+  },
+
+  toCurrency() {
+
+  },
+
+  fromCurrency() {
+
+  },
+
+  inverseCase() {
+
+  },
+
+  alternatingCase() {
+
+  },
+
+  getMiddle() {
+
+  },
+
+  numberWords() {
+
+  },
+
+  isDigit() {
+
+  },
+  doubleCheck() {
+
+  }
+}
+Object.assign( String.prototype, StringClass );
