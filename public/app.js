de6be649901fff4d92ac1fcc0a531b4e6c0f3303
andela-tisrael <israel.tomilayo@andela.com>
2017-01-06 09:48:24 +0100

    feature(frontend): Implement the front end of the string class

---
+++ public/app.js
@@ -0,0 +1,37 @@
+//var app = angular.module('StringClass',[]);
+angular.module('StringClass', [])
+.controller('StringCtrl', ['$scope', ($scope) => {
+    $scope.options = [
+      { id: 0, label: 'hasVowels'},
+      { id: 1, label: 'toUpper'},
+      { id: 2, label: 'toLower'},
+      { id: 3, label: 'ucFirst'},
+      { id: 4, label: 'isQuestion'},
+      { id: 5, label: 'word'},
+      { id: 6, label: 'wordCount'},
+      { id: 7, label: 'inverseCase'},
+      { id: 8, label: 'alternatingCase'},
+      { id: 9, label: 'getMiddle'},
+      { id: 10, label: 'numberWords'},
+      { id: 11, label: 'isDigit' },
+      { id: 12, label: 'doubleCheck' },
+      { id: 13, label: 'toCurrency' },
+      { id: 14, label: 'fromCurrency' }
+    ];
+
+    $scope.showResult = (stringQuery, selectedMethod) => {
+      if (selectedMethod === undefined) {
+        $scope.result = 'Select a Method';
+      }
+      else if (stringQuery === undefined) {
+        $scope.result = 'Type in a String in the input box';
+      }
+      else {
+        $scope.result = applyMethod(selectedMethod.label, stringQuery);
+      }
+    };
+
+    let applyMethod = (method, query) => {
+      return query[method]();
+    };
+    }]);
